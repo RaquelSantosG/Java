d5021ae63e88fbc1a30b0f333283d85481bb6141
raquelnew <raquelnew3@gmail.com>
2021-09-02 17:40:19 -0300

    for_while

---
+++ java_For_while_do_while/For.java
@@ -0,0 +1,34 @@
+package java_For_while_do_while;
+
+import java.util.Scanner;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		/*Informar todos os números de 1000 a 1999 que quando divididos por 11
+obtemos resto = 5. (FOR)*/
+		
+		float num=0;
+		int x=5;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		for (int i=1000; i < 1999; i++){
+		    if (i%11==5) {
+		        System.out.println(i);
+		    }
+		        System.out.printf("\nO resto é %d: %2.2f",x,num);
+		        
+				
+				
+
+		        
+		    }   
+		    
+		    }
+
+
+	}
+
+	
---
+++ java_For_while_do_while/par_impar.java
@@ -0,0 +1,39 @@
+package java_For_while_do_while;
+
+public class par_impar {
+
+	public static void main(String[] args) {
+		
+		int n=10,contpar=0,somaimpar=0,x=1;
+		
+		
+			System.out.println("\n digite um valor: ");
+			for(x=0;x<=1;x++)
+				
+			if(n % 2 == 1)
+	
+				
+				
+			{
+				contpar++;
+			}
+			else
+			{
+		somaimpar +=n;
+		{
+			contpar +=n;		
+		}
+		}
+			x++;
+		
+		
+		
+		System.out.println("\nQuantidade de pares:"+contpar);
+		System.out.println("\nQuantidade de impar:"+somaimpar);
+
+
+
+
+	}
+
+}
